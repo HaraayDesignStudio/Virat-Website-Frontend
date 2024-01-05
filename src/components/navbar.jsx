d8bedb53413450bcff4943d788648a7199b3faae
Ganesh Shinde <sindheganesh2004@gmail.com>
2024-01-05 22:52:10 +0530

    Created the first page

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,14 +1,43 @@
-import React from "react";
-import { IoReturnUpBack } from "react-icons/io5";
+import React, { useState } from "react";
+import { IoReturnUpBack, IoClose } from "react-icons/io5";
 import "./compenents.css";
 
 const Navbar = () => {
+  const [drawerOpen, setDrawerOpen] = useState(false);
+
+  const handleDrawerToggle = () => {
+    setDrawerOpen(!drawerOpen);
+  };
+
+  const handleCloseDrawer = () => {
+    setDrawerOpen(false);
+  };
+
+  const handleOverlayClick = (e) => {
+    // Close the drawer if clicked outside its content
+    if (e.target.classList.contains("drawer-overlay")) {
+      handleCloseDrawer();
+    }
+  };
+
   return (
     <div className="navbar-container">
-      <div className="navbar-menu">
-        <IoReturnUpBack className="navbar-icon-open" />
+      <span className="navbar-menu" onClick={handleDrawerToggle}>
+        <IoReturnUpBack
+          className={`navbar-icon-open ${drawerOpen ? "rotate" : ""}`}
+        />
         <p>Menu</p>
-      </div>
+      </span>
+
+      {drawerOpen && (
+        <div className="drawer-overlay open" onClick={handleOverlayClick}>
+          <div className="drawer-content open">
+            <IoClose className="close-icon" onClick={handleCloseDrawer} />
+            {/* Content of the Drawer */}
+            <p>Drawer Content</p>
+          </div>
+        </div>
+      )}
     </div>
   );
 };
--- src/pages/homePage.jsx
+++ src/pages/homePage.jsx
@@ -1,14 +1,30 @@
 import React from "react";
 import homepageimage from "../assets/image 8.png";
 import "./pages.css";
+import { IoIosArrowRoundForward } from "react-icons/io";
 
 const HomePage = () => {
   return (
     <div className="homepage-container">
       <div className="image-container-homepage">
         <img src={homepageimage} alt="homepage" />
-
-        <img src={homepageimage} alt="homepage" />
+        <p className="homepage-image-text">
+          Lorem Ipsum is simply dummy text of the <br /> printing and
+          typesetting industry. <br />
+          Lorem Ipsum has been the industry's standard <br /> dummy text ever
+          since the 1500s
+        </p>
+      </div>
+      <div className="homapage-headtext">
+        <span className="homepage-headtext-p">
+          Where Expertise Convergence with Experience
+        </span>
+        <div className="homepage-headtext-button">
+          <span className="homepage-headtext-content">
+            <p>Know more</p>
+            <IoIosArrowRoundForward className="homepage-headtext-icon" />
+          </span>
+        </div>
       </div>
     </div>
   );
