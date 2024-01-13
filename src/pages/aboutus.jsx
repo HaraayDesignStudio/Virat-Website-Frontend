9002b8d175efb7d8d376a182cfa386fe23c020ce
Ganesh Shinde <sindheganesh2004@gmail.com>
2024-01-13 13:24:14 +0530

    Created the vertical scroller

--- src/pages/aboutus.jsx
+++ src/pages/aboutus.jsx
@@ -1,5 +1,8 @@
 import React from "react";
-import img1 from "../assets/Rectangle 162.png"
+import img1 from "../assets/Rectangle 162.png";
+import movimg1 from "../assets/about/14_HVAC-MEP_143280593 1.png";
+import movimg2 from "../assets/about/picxy_DD228481 2.png";
+import movimg3 from "../assets/about/team-1 1.png";
 
 const Aboutus = () => {
   return (
@@ -14,7 +17,7 @@ const Aboutus = () => {
           </p>
         </div>
         <div className="about-one-img">
-            <img src={img1} alt="img1" />
+          <img src={img1} alt="img1" />
         </div>
         <div className="about-mission">
           <p className="about-mission-heading">Mision</p>
@@ -22,6 +25,73 @@ const Aboutus = () => {
             Our mission is to be the ultimate stop for facility management
             services.
           </p>
+        </div>
+      </div>
+      <div className="about-story-container">
+        <div className="about-story-content">
+          <p className="story-heading">Our Story</p>
+          <p className="story-disc">
+            At Virat, we understand the importance of efficient facility
+            management in ensuring the smooth operation and success of your
+            business. With our comprehensive range of services, we are committed
+            to providing you with top-notch solutions tailored to meet your
+            specific needs.At Virat, we understand the importance of efficient
+            facility management in ensuring the smooth operation and success of
+            your business. With our comprehensive range of services, we are
+            committed to providing you with top-notch solutions tailored to meet
+            your specific needs.At Virat, we understand the importance of
+            efficient facility management in ensuring the smooth operation and
+            success of your business. With our comprehensive range of services,
+            we are committed to providing you with top-notch solutions tailored
+            to meet your specific needs.At Virat, we understand the importance
+            of efficient facility management in ensuring the smooth operation
+            and success of your business. With our comprehensive range of
+            services, we are committed to providing you with top-notch solutions
+            tailored to meet your specific needs.At Virat, we understand the
+            importance of efficient facility management in ensuring the smooth
+            operation and success of your business. With our comprehensive range
+            of services, we are committed to providing you with top-notch
+            solutions tailored to meet your specific needs.
+          </p>
+        </div>
+        <div className="story-image-vertical-moving">
+          <div className="image-move-heading">
+            <p>Together is how we make it happen.</p>
+          </div>
+          <div className="moving-div">
+            <div className="inner-content">
+              <div className="inner-slider">
+                <div className="one-image">
+                  <img src={movimg1} alt="mov" />
+                </div>
+                <div className="two-image">
+                  <img src={movimg2} alt="mov" />
+                  <img src={movimg2} alt="mov" />
+                </div>
+                <div className="one-image">
+                  <img src={movimg3} alt="mov" />
+                </div>
+                <div className="two-image">
+                  <img src={movimg2} alt="mov" />
+                  <img src={movimg2} alt="mov" />
+                </div>
+                <div className="one-image">
+                  <img src={movimg1} alt="mov" />
+                </div>
+                <div className="two-image">
+                  <img src={movimg2} alt="mov" />
+                  <img src={movimg2} alt="mov" />
+                </div>
+                <div className="one-image">
+                  <img src={movimg3} alt="mov" />
+                </div>
+                <div className="two-image">
+                  <img src={movimg2} alt="mov" />
+                  <img src={movimg2} alt="mov" />
+                </div>
+              </div>
+            </div>
+          </div>
         </div>
       </div>
     </div>
