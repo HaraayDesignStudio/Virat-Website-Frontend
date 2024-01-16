763f139fc6b3ec90c73788899f8196dba58ec0e2
Ganesh Shinde <sindheganesh2004@gmail.com>
2024-01-16 22:22:02 +0530

    Created the footer for mobile

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -3,17 +3,60 @@ import FooterBack from "../assets/footer.png";
 import "./compenents.css";
 import { FaArrowDownLong } from "react-icons/fa6";
 import FooterCom from "./footerComponents";
+import footermobile from "../assets/footer-mobile.png";
 import { FaLinkedin } from "react-icons/fa";
 import { IoLogoWhatsapp } from "react-icons/io";
+import { Link } from "react-router-dom";
 
 const Footer = () => {
   return (
-    <div className="footer-wrapper">
-      <img src={FooterBack} alt="footer" className="footer-background" />
-      <div className="footer-content">
-        <div className="footer-left">
-          <p className="footer-get-in-touch">Get in Touch</p>
-          <p className="footer-disc">
+    <>
+      <div className="footer-wrapper">
+        <img src={FooterBack} alt="footer" className="footer-background" />
+        <div className="footer-content">
+          <div className="footer-left">
+            <p className="footer-get-in-touch">Get in Touch</p>
+            <p className="footer-disc">
+              Lorem Ipsum is simply dummy text of the printing and typesetting
+              industry. Lorem Ipsum has been the industry's standard dummy text
+              ever since{" "}
+            </p>
+            <div className="footer-download">
+              <FaArrowDownLong className="footer-download-icon" />
+              <span>Download brochure</span>
+            </div>
+          </div>
+          <div className="footer-right">
+            <div className="footer-box">
+              <FooterCom name={"Email us"} link={""} />
+              <FooterCom name={"Visit us"} link={""} />
+            </div>
+            <div className="footer-social">
+              <p className="socialmedia-head">Socialmedia</p>
+              <div className="socialmedia-icons">
+                <p>
+                  LinkedIn <FaLinkedin />
+                </p>
+                <p>
+                  WhatsApp <IoLogoWhatsapp />
+                </p>
+              </div>
+            </div>
+          </div>
+          <div className="logo-name-div">
+            <p className="virat-logo-name">Virat</p>
+          </div>
+        </div>
+      </div>
+      <div className="footer-wrapper-mobile">
+        <div className="footer-image-mobile">
+          <img src={footermobile} alt="footerimage" />
+          <p className="footer-mobile-heading">
+            Where Expertise Convergence with Experience
+          </p>
+        </div>
+        <div className="footer-mobile-content">
+          <p className="footer-mobile-text">
             Lorem Ipsum is simply dummy text of the printing and typesetting
             industry. Lorem Ipsum has been the industry's standard dummy text
             ever since{" "}
@@ -22,29 +65,19 @@ const Footer = () => {
             <FaArrowDownLong className="footer-download-icon" />
             <span>Download brochure</span>
           </div>
-        </div>
-        <div className="footer-right">
-          <div className="footer-box">
-            <FooterCom name={"Email us"} link={""} />
-            <FooterCom name={"Visit us"} link={""} />
+          <div className="hr-div"></div>
+          <p className="footer-mobile-getintouch">Get in touch</p>
+          <p className="footer-mobile-email">[1] Email Us</p>
+          <p className="footer-mobile-email">[2] Visit Us</p>
+          <div className="hr-div"></div>
+          <p className="follow-us-on">Follow us on</p>
+          <div className="links-mobile">
+            <Link className="link-mobile" to="/"></Link>
+            <Link className="link-mobile" to="/"></Link>
           </div>
-          <div className="footer-social">
-            <p className="socialmedia-head">Socialmedia</p>
-            <div className="socialmedia-icons">
-              <p>
-                LinkedIn <FaLinkedin />
-              </p>
-              <p>
-                WhatsApp <IoLogoWhatsapp />
-              </p>
-            </div>
-          </div>
-        </div>
-        <div className="logo-name-div">
-          <p className="virat-logo-name">Virat</p>
         </div>
       </div>
-    </div>
+    </>
   );
 };
 
